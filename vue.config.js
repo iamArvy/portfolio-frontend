1c43fe52218c1866b384e4ac89c021f0e3e98cfc
Oke Marvellous <okeseyi5@gmail.com>
2024-06-01 15:40:18 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/iamarvy.github.io/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/portfolio-frontend/'
+    : '/'
 })
